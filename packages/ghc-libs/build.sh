593c84c85aee8c0d8e21b74260f606ea2ad9b28c
Aditya Alok <alok@termux.dev>
2025-02-26 04:04:03 +0530

    Final blow
    Signed-off-by: Aditya Alok <alok@termux.dev>

--- packages/ghc-libs/build.sh
+++ packages/ghc-libs/build.sh
@@ -43,7 +43,8 @@ termux_step_make() {
 		unset CFLAGS CPPFLAGS LDFLAGS # For stage0 compilation.
 		./hadrian/build binary-dist-dir -j"$TERMUX_PKG_MAKE_PROCESSES" --flavour="$flavour" --docs=none \
 			"stage1.unix.ghc.link.opts += -optl-landroid-posix-semaphore" \
-			"stage2.unix.ghc.link.opts += -optl-landroid-posix-semaphore"
+			"stage2.unix.ghc.link.opts += -optl-landroid-posix-semaphore" \
+			"stage2.haddock.ghc.link.opts += -optl-landroid-posix-semaphore"
 	)
 }
 
@@ -55,7 +56,6 @@ termux_step_make_install() {
 	./configure \
 		--prefix="$TERMUX_PREFIX" \
 		--with-system-libffi \
-		--disable-ld-override \
 		--host="$target"
 
 	HOST_GHC_PKG="$(realpath ../../stage1/bin/"$target"-ghc-pkg)" make install
@@ -66,40 +66,40 @@ termux_step_install_license() {
 		"$TERMUX_PKG_SRCDIR/LICENSE"
 }
 
-termux_step_post_massage() {
-	# Remove cross-prefix from binaries and fix links:
-	for path in bin/"$target"-*; do
-		newpath="${path//$target-/}"
-
-		if [ -h "$path" ]; then
-			link_target="$(readlink "$path")"
-			ln -sf "${link_target//$target-/}" "$newpath"
-			rm "$path"
-		else
-			mv "$path" "$newpath"
-		fi
-
-	done
-
-	local ghclibs_dir="lib/$target-ghc-$TERMUX_PKG_VERSION"
-
-	if ! [ -d "$ghclibs_dir" ]; then
-		echo "ERROR: GHC lib directory is not at expected place. Please verify before continuing."
-		exit 1
-	fi
-
-	# We may build GHC with `{llc,opt}-<version suffix>`, but only `llc`,`opt` is present in Termux:
-	sed -i -E 's|("LLVM llc command",) "llc.*"|\1 "llc"|' "$ghclibs_dir"/lib/settings
-	sed -i -E 's|("LLVM opt command",) "opt.*"|\1 "opt"|' "$ghclibs_dir"/lib/settings
-
-	# Remove cross-prefix from settings:
-	# Why? Since we will use native compiler on the target, we don't need them.
-	sed -i "s|${target}-||g" "$ghclibs_dir"/lib/settings
-
-	# The second `configure` script was ment to be run on host(=target) but we are running
-	# on CI, therefore we need to remove cross compiler flag:
-	sed -i -E 's|("cross compiling",) "YES"|\1 "NO"|' "$ghclibs_dir"/lib/settings
-
-	find . -type f \( -name "*.so" -o -name "*.a" \) -exec "$STRIP" --strip-unneeded {} \;
-	find "$ghclibs_dir"/bin -type f -exec "$STRIP" {} \;
-}
+# termux_step_post_massage() {
+# 	# Remove cross-prefix from binaries and fix links:
+# 	for path in bin/"$target"-*; do
+# 		newpath="${path//$target-/}"
+#
+# 		if [ -h "$path" ]; then
+# 			link_target="$(readlink "$path")"
+# 			ln -sf "${link_target//$target-/}" "$newpath"
+# 			rm "$path"
+# 		else
+# 			mv "$path" "$newpath"
+# 		fi
+#
+# 	done
+#
+# 	local ghclibs_dir="lib/$target-ghc-$TERMUX_PKG_VERSION"
+#
+# 	if ! [ -d "$ghclibs_dir" ]; then
+# 		echo "ERROR: GHC lib directory is not at expected place. Please verify before continuing."
+# 		exit 1
+# 	fi
+#
+# 	# We may build GHC with `{llc,opt}-<version suffix>`, but only `llc`,`opt` is present in Termux:
+# 	sed -i -E 's|("LLVM llc command",) "llc.*"|\1 "llc"|' "$ghclibs_dir"/lib/settings
+# 	sed -i -E 's|("LLVM opt command",) "opt.*"|\1 "opt"|' "$ghclibs_dir"/lib/settings
+#
+# 	# Remove cross-prefix from settings:
+# 	# Why? Since we will use native compiler on the target, we don't need them.
+# 	sed -i "s|${target}-||g" "$ghclibs_dir"/lib/settings
+#
+# 	# The second `configure` script was ment to be run on host(=target) but we are running
+# 	# on CI, therefore we need to remove cross compiler flag:
+# 	sed -i -E 's|("cross compiling",) "YES"|\1 "NO"|' "$ghclibs_dir"/lib/settings
+#
+# 	find . -type f \( -name "*.so" -o -name "*.a" \) -exec "$STRIP" --strip-unneeded {} \;
+# 	find "$ghclibs_dir"/bin -type f -exec "$STRIP" {} \;
+# }
